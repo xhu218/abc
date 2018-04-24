50e6bd89b424a8a2113f39f8fe1d33fa8e275900
WangFugui <WangFugui@DESKTOP-4DA1OTK>
2018-04-24 20:56:11 +0800

    fffffffff

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using 
-namespace csharp
+
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); 
+            Console.WriteLine("Hello World!");
         }
     }
 }
